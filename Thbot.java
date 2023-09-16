bda30e8b8a2e9d7221897c6ddb5592ccdb67ea0f
EOTH04 <106122454+EOTH04@users.noreply.github.com>
2023-09-15 22:24:51 -0300

    robô e código
    package thdaspaty;
    import robocode.*;
    import java.awt.Color;
    
    /**
     * thbot - a class by (robô de th)
     */
    public class Thbot extends Robot{
    public void run (){
    	setColors(Color.green,Color.black,Color.blue,Color.red,Color.green);
    	while (true) {
    			ahead(100);
    			turnRight(90);
    			ahead(100);
    			turnLeft(90);
    			ahead(100);
    		}
    	}
    	public void onScannedRobot (ScannedRobotEvent e){
    	String robotank = e.getName();
    	double distancia = e.getDistance();
    	System.out.println(robotank + " distancia " + distancia);
    	if (distancia < 135)	{
    		fire(3);
    	}	else {
    		fire(1);
    	}
    	//fire(1.7);
    	}
    	public void onHitWall(HitWallEvent e){
    		back(50);
    		turnRight(90);
    	}
    }

---
+++ Thbot.java
@@ -0,0 +1,34 @@
+package thdaspaty;
+import robocode.*;
+import java.awt.Color;
+
+/**
+ * thbot - a class by (robô de th)
+ */
+public class Thbot extends Robot{
+public void run (){
+	setColors(Color.green,Color.black,Color.blue,Color.red,Color.green);
+	while (true) {
+			ahead(100);
+			turnRight(90);
+			ahead(100);
+			turnLeft(90);
+			ahead(100);
+		}
+	}
+	public void onScannedRobot (ScannedRobotEvent e){
+	String robotank = e.getName();
+	double distancia = e.getDistance();
+	System.out.println(robotank + " distancia " + distancia);
+	if (distancia < 135)	{
+		fire(3);
+	}	else {
+		fire(1);
+	}
+	//fire(1.7);
+	}
+	public void onHitWall(HitWallEvent e){
+		back(50);
+		turnRight(90);
+	}
+}
